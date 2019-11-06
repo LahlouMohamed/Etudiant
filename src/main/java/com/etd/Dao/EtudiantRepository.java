e1a0a81bb027445c550cabdc08a3df5034eed76c
LahlouMohamed <lahloumohamed0901@gmail.com>
2019-11-07 00:04:38 +0100

    test git local ssh

--- src/main/java/com/etd/Dao/EtudiantRepository.java
+++ src/main/java/com/etd/Dao/EtudiantRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.etd.Entities.Etudiant;
 
-public interface EtudiantRepository extends JpaRepository<Etudiant, Long> {
+public interface EtudiantRepository extends JpaRepository<Etudiant, Long> 
+{
 
 }
